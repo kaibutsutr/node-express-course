67aac0122615c170ebf07533a2151e85b2b08ce5
kaibutsutr <76039421+kaibutsutr@users.noreply.github.com>
2024-08-06 14:18:02 +0300

    12121

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,7 @@
-console.log('Express Tutorial')
+const http = require("http"); //create http obj
+const server = http.createServer((req, res) => {
+  //create a server
+  console.log("user hit the server");
+  res.end("Home Page"); // every server must have a response end point
+});
+server.listen(5000); // work on port 5000 its not taken
