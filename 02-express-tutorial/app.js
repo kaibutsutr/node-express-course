ded833e06b1103fe9461db91e7468bf5819d04cc
kaibutsutr <76039421+kaibutsutr@users.noreply.github.com>
2024-08-07 13:52:52 +0300

    1212

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const path = require("path"); //node path for files
 const port = 3000;
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
@@ -10,13 +11,14 @@ app.listen(port, () => {
 app.use(express.static("./public"));
 // put your static files here in this folder. static means server wont change it. Like css,image,logo and js files.
 
-app.get("/", (req, res) => {
-  res.status(200).send("Homepage");
-});
+// app.get("/", (req, res) => {
+//   //res.status(200).send("Homepage");
+//   res.sendFile(path.resolve(__dirname, "./public/index.html")); //mainpage brings a file and its css js files are held in public folder
+// });
 
-app.get("/about", (req, res) => {
-  res.status(200).send("About");
-});
+// app.get("/about", (req, res) => {
+//   res.status(200).send("About");
+// });
 app.all("*", (req, res) => {
   res.status(404).send("Page not Found!!!"); //200 success 404 failure
 });
