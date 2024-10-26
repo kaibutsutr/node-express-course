296fd1bec9455b8055c5ea474996dc492efe1fd3
kaibutsutr <76039421+kaibutsutr@users.noreply.github.com>
2024-10-26 23:40:03 +0300

    123

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -5,15 +5,27 @@ const { people } = require("./data"); // import people array
 
 app.use(express.static("./methods-public")); // use these static files for the app
 app.use(express.urlencoded({ extended: false })); // use urlencoder middleware to parse html body for data
+app.use(express.json()); // this middleware parses data from JSON file type.
+
 app.get("/api/people", (req, res) => {
   res.status(200).json({ success: true, data: people }); //send json object with success true and people array
 });
-app.post("/login", (req, res) => {
-  const { name } = req.body; // parse name value from body
+
+app.post("/api/people", (req, res) => {
+  const { name } = req.body; // destructure name property from the body.
   if (name) {
-    res.status(200).send(`Welcome ${name}`);
+    res.status(201).json({ success: true, person: name }); // 201 is successfull post request. Send a Json file with new persons name
   } else {
-    res.status(401).send("Please enter your login info");
+    res.status(400).json({ success: false, msg: "please provide a name" }); // if form is empty, throw an error message in your req.body file
+  }
+});
+
+app.post("/login", (req, res) => {
+  const { name } = req.body; // destructure name property from the body. If exists then send welcome name, if not then send error.
+  if (name) {
+    res.status(200).send(`Login successful! Welcome ${name}`);
+  } else {
+    res.status(401).send("Login failed! Please enter your login info");
   }
 });
 app.listen(port, () => {
